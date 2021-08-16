7133949afb65367fad893a29375b300c34bc1e98
Fernando Martinez <fernando817mm@gmail.com>
2021-08-16 16:56:13 -0500

    mvp done

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,20 +1,57 @@
+import React from 'react';
+import { render, screen, waitFor} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Display from '../Display'
 
 
+const testDisplay = {
+    name: "Stranger Things",
+    summary:"A great show",
+    seasons:[
+        {id:0, name:"Season 2", episodes: []}
+    ]
+}
+const displayFunc = (data) => {
+    console.log(data);
+}
 
+test('renders without any props', () => {
+    render(<Display/>)
+});
 
+test('test component showing when button is pressed', () => {
+    render(<Display displayFunc={displayFunc}/>)
 
+    const show = screen.queryByTestId('show-container');
+    waitFor(() => expect(show).toBeInTheDocument());
 
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+});
 
+test('test seasons to have right # of ep',() => {
+    render (<Display show = {testDisplay}/>);
 
+    const button = screen.getByRole('button');
+    userEvent.click(button);
 
+    waitFor(() => expect(screen.getAllByTestId('season-option')).toHaveLength(testDisplay.seasons.length));
+});
 
+test('test if optional function is being called', () => {
+    const mockClick = jest.fn();
 
+    render(<Display handleClick = {mockClick}/>);
 
+    const button = screen.getByRole('button');
+    userEvent.click(button);
 
+    waitFor(() => expect(mockClick).toHaveBeenCalledTimes(1));
+})
 
 
 ///Tasks:
-//1. Add in nessisary imports and values to establish the testing suite.
+//1. Add in necessary imports and values to establish the testing suite.
 //2. Test that the Display component renders without any passed in props.
 //3. Rebuild or copy a show test data element as used in the previous set of tests.
 //4. Test that when the fetch button is pressed, the show component will display. Make sure to account for the api call and change of state in building your test.
